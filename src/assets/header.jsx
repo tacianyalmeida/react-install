e4d47062e0e4f01a99ce5a83e3a969706668085f
Taciany Maria Santos de Almeida <138811627+tacianyalmeida@users.noreply.github.com>
2024-03-15 18:30:02 -0300

    Add files via upload

---
+++ src/assets/header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+const Header = () => {
+    return <header className="cabecalho">
+        <p>Seja Bem-Vindo</p>
+       <a href="#"></a>
+       <a href="#"></a>
+        </header>
+}
+
+export default Header 
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Header from './assets/header'
+import Section from './section'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <Header/>
+  <Section/>
+  </React.StrictMode>,
+)
+
+
